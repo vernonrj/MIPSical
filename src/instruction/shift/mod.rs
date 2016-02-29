896d4b1b3564d01997d3bc03aa6d28daaf4d92b7
Vernon Jones <vernonrjones@gmail.com>
2016-02-28 16:21:12 -0800

    Added another instruction

--- src/instruction/shift/mod.rs
+++ src/instruction/shift/mod.rs
@@ -4,11 +4,14 @@ use decoded::{Opcode, Decodable, Decoded};
 
 mod sll;
 mod srl;
+mod sra;
 
 pub use self::sll::SLL;
 pub use self::srl::SRL;
+pub use self::sra::SRA;
 
 pub fn register(m: &mut HashMap<Opcode, Box<Fn(u32) -> Box<Decoded> + 'static>>) {
     m.insert(SLL::opcode(), Box::new(|opcode| Box::new(SLL::new(opcode))));
     m.insert(SRL::opcode(), Box::new(|opcode| Box::new(SRL::new(opcode))));
+    m.insert(SRA::opcode(), Box::new(|opcode| Box::new(SRA::new(opcode))));
 }
--- src/instruction/shift/sra.rs
+++ src/instruction/shift/sra.rs
@@ -1,17 +1,17 @@
-// Shift Word Right Logical
+// Shift Word Right Arithmetic
 use decoded::{IO, Opcode, Decodable, Decoded};
 use decoder::*;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub struct SRL {
+pub struct SRA {
     rt: u8,
     rd: u8,
     sa: u8,
 }
 
-impl SRL {
+impl SRA {
     pub fn new(bitfield: u32) -> Self {
-        SRL {
+        SRA {
             rt: extract_20_16(bitfield),
             rd: extract_15_11(bitfield),
             sa: extract_10_6(bitfield),
@@ -19,15 +19,15 @@ impl SRL {
     }
 }
 
-impl Decodable for SRL {
+impl Decodable for SRA {
     fn opcode() -> Opcode {
-        Opcode::Special(0b000010)
+        Opcode::Special(0b000011)
     }
 }
 
-impl Decoded for SRL {
+impl Decoded for SRA {
     fn name(&self) -> &'static str {
-        "SRL"
+        "SRA"
     }
     fn inputs(&self) -> Vec<IO> {
         vec![IO::Register(self.rt)]
@@ -45,15 +45,15 @@ impl Decoded for SRL {
 
 
 #[test]
-fn srl_decode() {
-    assert_eq!(SRL::new(0b000000_00000_11111_00000_11111_000000),
-               SRL {
+fn sra_decode() {
+    assert_eq!(SRA::new(0b000000_00000_11111_00000_11111_000000),
+               SRA {
                    rt: 0b11111,
                    rd: 0b00000,
                    sa: 0b11111,
                });
-    assert_eq!(SRL::new(0b000000_00000_00000_11111_00000_000000),
-               SRL {
+    assert_eq!(SRA::new(0b000000_00000_00000_11111_00000_000000),
+               SRA {
                    rt: 0b00000,
                    rd: 0b11111,
                    sa: 0b00000,
@@ -61,12 +61,12 @@ fn srl_decode() {
 }
 
 #[test]
-fn srl_exec() {
-    let s = SRL {
+fn sra_exec() {
+    let s = SRA {
         rt: 0,
         rd: 1,
         sa: 2,
     };
-    assert_eq!(s.execute(&[10]), Some(2));
+    assert_eq!(s.execute(&[50]), Some(12));
 }
 
